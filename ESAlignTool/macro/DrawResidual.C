5effb4b54c58b1f88f7a2cbebf94e9fd7c806b45
Alpha <jui-fa.tsai@cern.ch>
2014-10-02 16:27:14 +0200

    Add macro for ploting

--- ESAlignTool/macro/DrawResidual.C
+++ ESAlignTool/macro/DrawResidual.C
@@ -13,9 +13,9 @@
 #include "TStyle.h"
 #include "TROOT.h"
 #include "CMSstyle.C"
-#include "DrawFunResi.C"
+#include "PlotFunc.C"
 using namespace std;
-void AlignResi(){
+void DrawResidual(){
 	TCanvas* c1 = new TCanvas("c1","", 850, 700 ); 
 	TFile* fResidual = new TFile("/afs/cern.ch/work/j/jtsai/ESAlignment/CMSSW_7_0_7_dev/src/AlignmentTool/ESAlignTool/test/OnLxplus/02Oct_PoinGun1M_GTv5/job_SinglePionGun_GEN_SIM_v1__tdoan-job_SinglePionGun_RECO_v1-a2645d794d4263c6295ef839881df757__USER.root");	
 	//TFile* FourCor_i11_ESpF = new TFile("/Users/Alpha/MyAna/ESAlignMent/result/2011A_RawRECO_SigEle_Nov/4Corner/iter11/ESpF_4Corner_iter11.root");	
