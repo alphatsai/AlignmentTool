6273d450dee4e92a09b59e3bc2f7c52a86d9326c
Alpha <jui-fa.tsai@cern.ch>
2014-10-10 17:36:51 +0200

    Fix the problem of initialasion of ration matrix

---
+++ ESAlignTool/python/DefaultESLocation_cfi.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+DefaultESLocation = cms.PSet(
+	ESpF_X = cms.double(0), ESpF_Y = cms.double(0), ESpF_Z = cms.double(303.846), ESpF_Alpha = cms.double(0), ESpF_Beta = cms.double(0), ESpF_Gamma = cms.double(0),
+	ESpR_X = cms.double(0), ESpR_Y = cms.double(0), ESpR_Z = cms.double(308.306), ESpR_Alpha = cms.double(0), ESpR_Beta = cms.double(0), ESpR_Gamma = cms.double(0),	
+	ESmF_X = cms.double(0), ESmF_Y = cms.double(0), ESmF_Z = cms.double(-303.846), ESmF_Alpha = cms.double(0), ESmF_Beta = cms.double(0), ESmF_Gamma = cms.double(0),	
+	ESmR_X = cms.double(0), ESmR_Y = cms.double(0), ESmR_Z = cms.double(-308.306), ESmR_Alpha = cms.double(0), ESmR_Beta = cms.double(0), ESmR_Gamma = cms.double(0),	
+)
+
