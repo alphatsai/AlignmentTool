3d7fc7c2aa0fcb0ee7ca235f1586836238b9bf98
Alpha <jui-fa.tsai@cern.ch>
2015-02-27 16:40:53 +0100

    Update inputFile with compatible version samples

--- ESAlignTool/test/inputFiles_cfi.py
+++ ESAlignTool/test/inputFiles_cfi.py
@@ -5,7 +5,7 @@ FileNames_CSA14Test = [
 	'root://eoscms//eos/cms/store/mc/Spring14dr/DYJetsToLL_M-50_HT-400to600_Tune4C_13TeV-madgraph-tauola/AODSIM/PU_S14_POSTLS170_V6-v1/00000/AAF1CA5D-9600-E411-B0E7-0025907FD2B2.root',
 ]
 FileNames_PionGunTest = [
-	'root://eoscms//eos/cms/store/user/jtsai/test/SinglePionGun_RECO_POSTLS170_V5_MaxEvents1M/results_1_1_hRW.root',
+	'root://eoscms//eos/cms/store/user/jtsai/ESAlignment/CMSSW_7_4_0_pre6/SinglePionGun_RECO_MCRUN2_74_V1_MaxEvents1M/results_81_1_Vt5.root',
 ]
 FileNames_Skim721 = [
 	'root://eoscms//eos/cms/store/user/jtsai/ESAlignment/Skim_SinglePionGun_RECO_POSTLS170_V5_MaxEvents1M_721/ESSkim_8.root',
