f1a0145935556419146987e8dd33ac28f9a90a46
Alpha <jui-fa.tsai@cern.ch>
2015-11-21 02:07:58 +0100

    Add more information used

--- ESAlignTool/macro/stabilities.py
+++ ESAlignTool/macro/stabilities.py
@@ -3,6 +3,15 @@ import os, re, sys, shutil
 import math, time
 from ROOT import *
 
+### DB file base points
+X00={'mR':0.,        'mF':0.,        'pF':0.,       'pR':0.      }
+Y00={'mR':0.,        'mF':0.,        'pF':0.,       'pR':0.      }
+Z00={'mR':-308.306,  'mF':-303.846,  'pF':303.846,  'pR':308.306 }
+A00={'mR':0.,        'mF':0.,        'pF':0.,       'pR':0.      }
+B00={'mR':0.,        'mF':0.,        'pF':0.,       'pR':0.      }
+G00={'mR':0.,        'mF':0.,        'pF':0.,       'pR':0.      }
+
+### Starting points ( aligment tag IVO + base point on db )
 ## 1. 2015 summer propmt reco
 #X0={'mR':-0.06,    'mF':-0.05,    'pF':0.52,    'pR':0.54    }
 #Y0={'mR':-0.749,   'mF':-0.779,   'pF':-0.97,   'pR':-0.959  }
@@ -16,13 +25,23 @@ X0={'mR':-0.303,   'mF':-0.284,   'pF':0.324,   'pR':0.357   }
 Y0={'mR':-0.462,   'mF':-0.477,   'pF':-0.932,  'pR':-0.905  }
 Z0={'mR':-308.922, 'mF':-304.289, 'pF':304.167, 'pR':308.770 }
 A0={'mR':1.4,      'mF':1.4,      'pF':-1.3,    'pR':-1.7    }
-B0={'mR':0.5,     'mF':0.8,       'pF':0.1,     'pR':0.4     }
-G0={'mR':-0.,     'mF':0.6,       'pF':0.8,     'pR':0.6     }
+B0={'mR':0.5,      'mF':0.8,      'pF':0.1,     'pR':0.4     }
+G0={'mR':-0.,      'mF':0.6,      'pF':0.8,     'pR':0.6     }
 
-inputFile='inputMatrixElements_cfi.py'
+### Input files
+#inputFile='pdf/inputMatrixElementsRunB.txt'
+#inputFile='pdf/inputMatrixElementsRunC25ns.txt'
+inputFile='pdf/inputMatrixElementsRunDv3.txt'
+#inputFile='pdf/inputMatrixElementsRunDv4.txt'
+
+#output='pdf/Stabilities_RunB'
+#output='pdf/Stabilities_RunC25ns'
 output='pdf/Stabilities_RunDv3'
-print '>> [INFO] Read file '+inputFile
+#output='pdf/Stabilities_RunDv4'
 
+
+### Main functions
+print '>> [INFO] Read file '+inputFile
 lines0 = open(inputFile)
 lines1 = filter( None, ( line.strip() for line in lines0 ))                             # Remove empty line
 lines2 = filter( lambda x: not x.startswith('#'), ( line.strip() for line in lines1 )) # Remove # comments lines
@@ -132,9 +151,9 @@ for p in ['mR', 'mF', 'pF', 'pR']:
     setStyle( gX, 'dX(cm)',        'iteration', '', -0.05, 0.05)
     setStyle( gY, 'dY(cm)',        'iteration', '', -0.05, 0.05)
     setStyle( gZ, 'dZ(cm)',        'iteration', '', -0.05, 0.05)
-    setStyle( gA, 'd#alpha(mrad)', 'iteration', '', -0.05, 0.05)
-    setStyle( gB, 'd#beta(mrad)',  'iteration', '', -0.05, 0.05)
-    setStyle( gG, 'd#gamma(mrad)', 'iteration', '', -0.05, 0.05)
+    setStyle( gA, 'd#alpha(mrad)', 'iteration', '', -0.1,  0.1)
+    setStyle( gB, 'd#beta(mrad)',  'iteration', '', -0.1,  0.1)
+    setStyle( gG, 'd#gamma(mrad)', 'iteration', '', -0.1,  0.1)
 
     tl = TLine(1,0,size,0)
     tl.SetLineColor(2)
@@ -160,23 +179,60 @@ for p in ['mR', 'mF', 'pF', 'pR']:
 
 outputFile = open(output+'_FinalPositions.txt', 'w')
 
-print ">> [INFO] Un-Aligned P"  
-print "          %5s %5s %6s %8s %5s %5s %5s "%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma')
-outputFile.write("Un-Aligned:\n") 
-outputFile.write("%5s %5s %6s %8s %5s %5s %5s\n"%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma' ))
+### Print out base point on db
+print  ">> [INFO] Ideal P0"  
+print  "          %5s %6s %6s %8s %5s %5s %5s  "%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma' )
+outputFile.write("Ideal P0:\n") 
+outputFile.write("%5s %6s %6s %8s %5s %5s %5s\n"%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma' ))
 for p in ['mR', 'mF', 'pF', 'pR']:
-    print "          %5s %5.2f %6.3f %8.3f %5.1f %5.1f %5.1f "%(p, X0[p], Y0[p], Z0[p], A0[p], B0[p], G0[p])
-    outputFile.write("%5s %5.2f %6.3f %8.3f %5.1f %5.1f %5.1f\n"%( p, X0[p], Y0[p], Z0[p], A0[p], B0[p], G0[p] ))
+    print  "          %5s %6.3f %6.3f %8.3f %5.1f %5.1f %5.1f  "%( p, X00[p], Y00[p], Z00[p], A00[p], B00[p], G00[p] )
+    outputFile.write("%5s %6.3f %6.3f %8.3f %5.1f %5.1f %5.1f\n"%( p, X00[p], Y00[p], Z00[p], A00[p], B00[p], G00[p] ))
 
 outputFile.write("\n")
  
-print ">> [INFO] Aligned P-dP" 
-print "          %5s %5s %6s %8s %5s %5s %5s "%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma')
-outputFile.write("Aligned P-dP:\n") 
-outputFile.write("%5s %5s %6s %8s %5s %5s %5s\n"%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma' ))
+### Print out starting point  
+print  ">> [INFO] Un-Aligned P"  
+print  "          %5s %6s %6s %8s %5s %5s %5s  "%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma' )
+outputFile.write("Un-Aligned:\n") 
+outputFile.write("%5s %6s %6s %8s %5s %5s %5s\n"%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma' ))
 for p in ['mR', 'mF', 'pF', 'pR']:
-    print "          %5s %5.2f %6.3f %8.3f %5.1f %5.1f %5.1f "%(p, X[p], Y[p], Z[p], A[p], B[p], G[p])
-    outputFile.write("%5s %5.2f %6.3f %8.3f %5.1f %5.1f %5.1f\n"%( p, X[p], Y[p], Z[p], A[p], B[p], G[p] ))
+    print  "          %5s %6.3f %6.3f %8.3f %5.1f %5.1f %5.1f  "%( p, X0[p], Y0[p], Z0[p], A0[p], B0[p], G0[p] )
+    outputFile.write("%5s %6.3f %6.3f %8.3f %5.1f %5.1f %5.1f\n"%( p, X0[p], Y0[p], Z0[p], A0[p], B0[p], G0[p] ))
+
+outputFile.write("\n")
+ 
+### Print out aligned point  
+print  ">> [INFO] Aligned P-dP" 
+print  "          %5s %6s %6s %8s %5s %5s %5s "%(  'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma')
+outputFile.write("Aligned P-dP:\n") 
+outputFile.write("%5s %6s %6s %8s %5s %5s %5s\n"%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma' ))
+for p in ['mR', 'mF', 'pF', 'pR']:
+    print  "          %5s %6.3f %6.3f %8.3f %5.1f %5.1f %5.1f  "%( p, X[p], Y[p], Z[p], A[p], B[p], G[p] )
+    outputFile.write("%5s %6.3f %6.3f %8.3f %5.1f %5.1f %5.1f\n"%( p, X[p], Y[p], Z[p], A[p], B[p], G[p] ))
+
+outputFile.write("\n")
+
+### Print out aligned point w.r.t base point 
+print ">> [INFO] Aligned (P-dP)-P0" 
+print "          %5s %6s %6s %8s %5s %5s %5s "%(   'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma')
+outputFile.write("Aligned (P-dP)-P0:\n") 
+outputFile.write("%5s %6s %6s %8s %5s %5s %5s\n"%( 'plain', 'X(cm)', 'Y(cm)', 'Z(cm)', 'Alpha', 'Beta', 'Gamma' ))
+for p in ['mR', 'mF', 'pF', 'pR']:
+    print  "          %5s %6.3f %6.3f %8.3f %5.1f %5.1f %5.1f  "%( p, X[p]-X00[p], Y[p]-Y00[p], Z[p]-Z00[p], A[p]-A00[p], B[p]-B00[p], G[p]-G00[p] )
+    outputFile.write("%5s %6.3f %6.3f %8.3f %5.1f %5.1f %5.1f\n"%( p, X[p]-X00[p], Y[p]-Y00[p], Z[p]-Z00[p], A[p]-A00[p], B[p]-B00[p], G[p]-G00[p] ))
+
+outputFile.write("\n")
+
+### Print out info for macro 
+Axis={  'X':X,   'Y':Y,   'Z':Z,   'Alpha':A,   'Beta':B,   'Gamma':G   }
+Axis0={ 'X':X00, 'Y':Y00, 'Z':Z00, 'Alpha':A00, 'Beta':B00, 'Gamma':G00 }
+print  ">> [INFO] For producing db file macro" 
+print  "          %6s  %7s, %7s, %7s, %7s, %7s, %7s, %7s, %7s  "%( 'Axis', 'mR', 'mR', 'mF', 'mF', 'pF', 'pF', 'pR', 'pR')
+outputFile.write("Aligned (P-dP)-P0:\n") 
+outputFile.write("%6s  %7s, %7s, %7s, %7s, %7s, %7s, %7s, %7s\n"%( 'Axis', 'mR', 'mR', 'mF', 'mF', 'pF', 'pF', 'pR', 'pR'))
+for a in [ 'X', 'Y', 'Z', 'Alpha', 'Beta', 'Gamma' ]:
+    print  "          %6s  %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f  "%( a, Axis[a]['mR']-Axis0[a]['mR'], Axis[a]['mR']-Axis0[a]['mR'], Axis[a]['mF']-Axis0[a]['mF'], Axis[a]['mF']-Axis0[a]['mF'], Axis[a]['pF']-Axis0[a]['pF'], Axis[a]['pF']-Axis0[a]['pF'], Axis[a]['pR']-Axis0[a]['pR'], Axis[a]['pR']-Axis0[a]['pR'] )
+    outputFile.write("%6s  %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f, %7.4f\n"%( a, Axis[a]['mR']-Axis0[a]['mR'], Axis[a]['mR']-Axis0[a]['mR'], Axis[a]['mF']-Axis0[a]['mF'], Axis[a]['mF']-Axis0[a]['mF'], Axis[a]['pF']-Axis0[a]['pF'], Axis[a]['pF']-Axis0[a]['pF'], Axis[a]['pR']-Axis0[a]['pR'], Axis[a]['pR']-Axis0[a]['pR'] ))
 
 outputFile.close()
 
